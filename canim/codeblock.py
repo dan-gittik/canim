8c69083f3d09fad0a42d6d6b26f2228e10b8b810
Dan Gittik <dan.gittik@gmail.com>
2023-07-18 17:28:42 +0200

    Added basic animation.

--- canim/codeblock.py
+++ canim/codeblock.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from manim import Text, UL, DOWN, LEFT
+from manim import Text, UL, DOWN, LEFT, AddTextLetterByLetter
 
 
 class CodeBlock:
@@ -18,27 +18,40 @@ class CodeBlock:
     def config(self) -> CodeConfig:
         return self._scene.code_config
     
-    def append_line(self, text: str) -> CodeLine:
+    def insert_line(self, text: str, index: int) -> CodeLine:
         line = CodeLine(self, text)
-        self._lines.append(line)
-        self._animate_line_insertion(line)
+        self._lines.insert(index, line)
+        self._animate_line_insertion(line, index)
         return line
     
-    def _animate_line_insertion(self, line: CodeLine) -> None:
+    def prepend_line(self, text: str) -> CodeLine:
+        return self.insert_line(text, 0)
+    
+    def append_line(self, text: str) -> CodeLine:
+        return self.insert_line(text, len(self._lines))
+    
+    def _animate_line_insertion(self, line: CodeLine, index: int) -> None:
         mobject = Text(
             text = line.text,
             font = self.config.font,
             font_size = self.config.font_size,
         )
-        if line.index == 0:
+        if index == 0:
             mobject.move_to([self.x, self.y, 0], UL)
         else:
-            prev = self._lines[line.index - 1]
-            mobject.next_to(prev._mobject, DOWN, buff=self.config.line_gap)
-            mobject.align_to(prev._mobject, LEFT)
+            prev_line = self._lines[index - 1]
+            mobject.next_to(prev_line._mobject, DOWN, buff=self.config.line_gap)
+            mobject.align_to(prev_line._mobject, LEFT)
         line._mobject = mobject
         self._scene.wait(self.config.delay_before)
-        self._scene.add(mobject)
+        slide_down = []
+        height_diff = mobject.height + self.config.line_gap
+        for next_line in self._lines[index + 1:]:
+            slide_down.append(next_line._mobject.animate.shift(height_diff * DOWN))
+        if slide_down:
+            self._scene.play(*slide_down, run_time=self.config.slide_speed)
+        duration = len(line.text) * self.config.typing_speed
+        self._scene.play(AddTextLetterByLetter(mobject), run_time=duration)
         self._scene.wait(self.config.delay_after)
         self._scene.next_section()
 
